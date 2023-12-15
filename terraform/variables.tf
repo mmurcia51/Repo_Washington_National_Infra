17477fc796b247c304148d083e2cd7a7bd2c6765
mmurcia51 <112260542+mmurcia51@users.noreply.github.com>
2023-12-15 18:13:00 -0500

    Update variables.tf
    ok

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,14 +8,7 @@ variable "ecs_execution_role_name" {
   default     = "washington-nat-ecs-execution-role"
 }
 
-/*variable "access_key" {
-  description = "access_key"
-  type        = string
-  sensitive   = true
 
-  default     = ${{ secrets.AWS_ACCESS_KEY_ID }}
-
-}
 
 # Definición variables
 /*
@@ -24,11 +17,3 @@ variable "buckets_s3" {
   type        = string
 }
 */
-variable "secret_key" {
-  description = "secret_key"
-  type        = string
-  sensitive   = true
-
-  default     = ${{ secrets.AWS_SECRET_ACCESS_KEY 
-
-}*/

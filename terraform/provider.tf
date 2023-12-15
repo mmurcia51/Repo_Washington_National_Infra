3c7aeca8c66c0e2133669d18603e6b347dc642b7
mmurcia51 <112260542+mmurcia51@users.noreply.github.com>
2023-12-15 18:21:18 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 # provider "aws" {
 #   region     = "us-east-1"
 #   alias      = "virginia"
-#   access_key = "AKIA6CY4IDLETEVNMFW4"
-#   secret_key = "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"
+#   access_key = 
+#   secret_key = 
 # }
 
 terraform {

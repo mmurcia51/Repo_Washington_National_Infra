64881c1acedc4be899ec4dd0ba93fa2689e774c1
mmurcia51 <112260542+mmurcia51@users.noreply.github.com>
2023-12-18 14:28:21 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,20 @@ output "cf_domain_name" {
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
   description = "Domain name corresponding to the distribution"
 }
+
+output "name_ecs" {
+  description = "Nombre ECS"
+  value       = aws_ecs_cluster.washington_ecs.name
+}
+
+output "name_task_def" {
+  description = "Nombre Task definitio"
+  value       = aws_ecs_task_definition.washington_tsk.family
+}
+
+
+output "name_alb" {
+  description = "Nombre alb"
+  value       = aws_alb.washington_alb.name
+}
+
